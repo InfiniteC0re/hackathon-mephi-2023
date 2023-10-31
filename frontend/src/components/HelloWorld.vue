64128dc2c4875453e1b43861b229f40a3aaa2fa7
InfiniteC0re <inf.core0@gmail.com>
2023-10-31 17:22:46 +0500

    Add support for scss and sass

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,11 +1,12 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+<script>
+export default {
+  props: {
+    msg: String
+  },
+  data: () => ({
+    count: 0
+  })
+}
 </script>
 
 <template>
