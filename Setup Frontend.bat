ac77ad48711fd4ac80843f4701952fcfacbf86e6
InfiniteC0re <inf.core0@gmail.com>
2023-10-31 21:39:12 +0500

    Merge setup scripts into one script

--- Setup Frontend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd ./frontend
-call npm i -g yarn
-call yarn
-pause
--- Setup.bat
+++ Setup.bat
@@ -1,5 +1,11 @@
 @echo off
+
 python -m venv venv
 call ./venv/Scripts/activate.bat
 pip install -r requirements.txt
+
+cd ./frontend
+call npm i -g yarn
+call yarn
+
 pause
