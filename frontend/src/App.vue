d231e2b48ba34162c04380e10171af4589e82b80
InfiniteC0re <inf.core0@gmail.com>
2023-11-01 11:21:43 +0500

    Add loading indicator

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <Header />
-  <router-view v-if="isReady"></router-view>
+  <div class="wrap-content" v-if="isReady">
+    <Header />
+    <router-view></router-view>
+  </div>
+  <div class="wrap-loading" v-else>
+    <LoadingIndicator/>
+  </div>
 </template>
 
 <script>
+import LoadingIndicator from "@/components/LoadingIndicator.vue"
 import Header from "@/components/Header.vue"
 
 export default {
   components: {
-    Header
+    Header,
+    LoadingIndicator
   },
   data: () => ({
     isReady: false
@@ -32,4 +39,17 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrap-content {
+  width: 100vw;
+  height: 100%;
+}
+.wrap-loading {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,21 +1,12 @@
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="@assets/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="@assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-    </div>
-    <HelloWorld msg="Vite + Vue" />
+    
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
     components: {
-        HelloWorld
+        
     },
     mounted() {
         let redirectToLogin = !this.$store.state.authToken;
